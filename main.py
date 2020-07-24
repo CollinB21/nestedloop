a9485f976c9294f20149e484a6b91cdd61cb8e07
CollinB21 <collinbowser213@gmail.com>
2020-07-24 18:01:26 +0000

    added the other part

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ for i in range(6): #outer loop
   print("outer loop | i = " + str(1))
   for j in range(6): #inner loop
     print("inner loop | i =" + str(i) + " | j = " + str(j))
+
+for i in range(9): #outer loop
+  print("outer loop | i = " + str(1))
+  for j in range(9): #inner loop
+    print("inner loop | i = " + str(i) + " | j = " + str(j))
+
+    
